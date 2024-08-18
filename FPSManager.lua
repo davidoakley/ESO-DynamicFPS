86bd2ba6ae933dc2b4a36d102dad307ea2c0bac7
David Oakley <david@oakley.me>
2024-08-18 19:22:18 +0100

    AddOn name change, and use LibDebugLogger

--- FPSManager.lua
+++ FPSManager.lua
@@ -1,6 +1,8 @@
-IdleFPSSaver = {}
-IdleFPSSaver.name = "IdleFPSSaver"
-IdleFPSSaver.svName = "IdleFPSSaver_SavedVariables"
+FPSManager = {}
+FPSManager.name = "FPSManager"
+FPSManager.svName = "FPSManager_SavedVariables"
+
+local logger = LibDebugLogger(FPSManager.name)
 
 local ACTIVE_FPS = 60
 local ACTIVE_CHECK_DELAY_MS = 5 * 1000
@@ -17,24 +19,23 @@ local idleTimeMS = 0
 local hasFocus = true
 local isIdle = false
 local isInCombat = false
-local defaultLabelColour = nil
 
 local function setActive(active, callback)
   local newFPS
   local newDelayMS
-  -- local reason
+  local reason
   local r, g, b, a = 1, 1, 1, 1
 
   if active then
     if isInCombat then
       newFPS = COMBAT_FPS
       newDelayMS = 0
-      -- reason = "combat"
+      reason = "combat"
       r, g, b = 1, 0.5, 0.5
     else
       newFPS = ACTIVE_FPS
       newDelayMS = ACTIVE_CHECK_DELAY_MS
-      -- reason = "active"
+      reason = "active"
       r, g, b, a = ZO_TOOLTIP_DEFAULT_COLOR:UnpackRGB()
     end
     idleTimeMS = 0
@@ -42,18 +43,18 @@ local function setActive(active, callback)
   else
     newFPS = IDLE_FPS
     newDelayMS = IDLE_CHECK_DELAY_MS
-    -- reason = "idle"
+    reason = "idle"
     r, g, b = 0.75, 1, 0.65
   end
   if newFPS ~= currentFPS then
-    EVENT_MANAGER:UnregisterForUpdate(IdleFPSSaver.name.."_CheckIdle")
+    EVENT_MANAGER:UnregisterForUpdate(FPSManager.name.."_CheckIdle")
     SetCVar("MinFrameTime.2", ""..(1 / newFPS))
     ZO_PerformanceMetersFramerateMeterLabel:SetColor(r, g, b, a)
     if newDelayMS > 0 then
-      -- d("IdleFPSSaver: Setting FPS to "..reason..": "..newFPS.."fps, idle check every "..newDelayMS.."ms")
-      EVENT_MANAGER:RegisterForUpdate(IdleFPSSaver.name.."_CheckIdle", newDelayMS, callback)
-    -- else
-    --   d("IdleFPSSaver: Setting FPS to "..reason..": "..newFPS.."fps")
+      logger:Debug("FPSManager: Setting FPS to "..reason..": "..newFPS.."fps, idle check every "..newDelayMS.."ms")
+      EVENT_MANAGER:RegisterForUpdate(FPSManager.name.."_CheckIdle", newDelayMS, callback)
+    else
+      logger:Debug("FPSManager: Setting FPS to "..reason..": "..newFPS.."fps")
     end
     currentFPS = newFPS
   end
@@ -62,7 +63,7 @@ end
 local x_old, y_old, h_old = GetMapPlayerPosition("player")
 local heading_old = GetPlayerCameraHeading()
 local function IdleCheck()
-    -- d("IdleFPSSaver: IdleCheck")
+    -- d("FPSManager: IdleCheck")
 
     local function IsPlayerIdle()
         -- Check if in combat
@@ -82,14 +83,14 @@ local function IdleCheck()
     if not IsPlayerIdle() then
       idleTimeMS = 0
       isIdle = false
-      -- d("IdleFPSSaver: not idle")
+      -- d("FPSManager: not idle")
       if hasFocus then
         setActive(true, IdleCheck)
       end
     else
       if not isIdle then
         idleTimeMS = idleTimeMS + ACTIVE_CHECK_DELAY_MS
-        -- d("IdleFPSSaver: Idle "..idleTimeMS.."ms")
+        -- d("FPSManager: Idle "..idleTimeMS.."ms")
         if idleTimeMS >= IDLE_DELAY_MS then
           isIdle = true
           if hasFocus then
@@ -97,7 +98,7 @@ local function IdleCheck()
           end
         end
       -- else
-      --   d("IdleFPSSaver: already idle")
+      --   d("FPSManager: already idle")
       end
     end
 end
@@ -111,45 +112,32 @@ local function setActiveTrue()
   setActive(true, IdleCheck)
 end
 
-function IdleFPSSaver.Initialize()
-  -- IdleFPSSaver.saved = ZO_SavedVars:NewAccountWide(IdleFPSSaver.svName, 1, nil, IdleFPSSaver.default)
+function FPSManager.Initialize()
+  -- FPSManager.saved = ZO_SavedVars:NewAccountWide(FPSManager.svName, 1, nil, FPSManager.default)
 
-  d("IdleFPSSaver initialising")
+  logger:Debug("FPSManager initialising")
   setActive(true, IdleCheck)
 
-  EVENT_MANAGER:RegisterForEvent(IdleFPSSaver.name.."_Combat", EVENT_PLAYER_COMBAT_STATE, OnCombatState)
+  EVENT_MANAGER:RegisterForEvent(FPSManager.name.."_Combat", EVENT_PLAYER_COMBAT_STATE, OnCombatState)
 
-  EVENT_MANAGER:RegisterForEvent(IdleFPSSaver.name.."_UIMovement", EVENT_NEW_MOVEMENT_IN_UI_MODE, setActiveTrue)
-  EVENT_MANAGER:RegisterForEvent(IdleFPSSaver.name.."_MouseDown", EVENT_GLOBAL_MOUSE_DOWN, setActiveTrue)
-  EVENT_MANAGER:RegisterForEvent(IdleFPSSaver.name.."MouseUp", EVENT_GLOBAL_MOUSE_UP, setActiveTrue)
-
-  -- EVENT_MANAGER:RegisterForEvent(
-	--   IdleFPSSaver.name.."_FocusChanged",
-	--   EVENT_GAME_FOCUS_CHANGED,
-	--   function(_, focus)
-  --     hasFocus = focus
-	--   end
-  -- )
+  EVENT_MANAGER:RegisterForEvent(FPSManager.name.."_UIMovement", EVENT_NEW_MOVEMENT_IN_UI_MODE, setActiveTrue)
+  EVENT_MANAGER:RegisterForEvent(FPSManager.name.."_MouseDown", EVENT_GLOBAL_MOUSE_DOWN, setActiveTrue)
+  EVENT_MANAGER:RegisterForEvent(FPSManager.name.."MouseUp", EVENT_GLOBAL_MOUSE_UP, setActiveTrue)
 end
 
 
 -- Then we create an event handler function which will be called when the "addon loaded" event
 -- occurs. We'll use this to initialize our addon after all of its resources are fully loaded.
-function IdleFPSSaver.OnAddOnLoaded(event, addonName)
+function FPSManager.OnAddOnLoaded(event, addonName)
   -- The event fires each time *any* addon loads - but we only care about when our own addon loads.
-  if addonName ~= IdleFPSSaver.name then return end
+  if addonName ~= FPSManager.name then return end
 
-  IdleFPSSaver.Initialize()
+  FPSManager.Initialize()
   --unregister the event again as our addon was loaded now and we do not need it anymore to be run for each other addon that will load
-  EVENT_MANAGER:UnregisterForEvent(IdleFPSSaver.name, EVENT_ADD_ON_LOADED) 
+  EVENT_MANAGER:UnregisterForEvent(FPSManager.name, EVENT_ADD_ON_LOADED) 
 end
 
 -- Finally, we'll register our event handler function to be called when the proper event occurs.
 -->This event EVENT_ADD_ON_LOADED will be called for EACH of the addns/libraries enabled, this is why there needs to be a check against the addon name
 -->within your callback function! Else the very first addon loaded would run your code + all following addons too.
-EVENT_MANAGER:RegisterForEvent(IdleFPSSaver.name, EVENT_ADD_ON_LOADED, IdleFPSSaver.OnAddOnLoaded)
-
--- EVENT_MANAGER:RegisterForEvent("beginCrafting", EVENT_CRAFTING_STATION_INTERACT, onCraftInteract)
--- EVENT_MANAGER:RegisterForEvent("itemGet", EVENT_INVENTORY_SINGLE_SLOT_UPDATE, onItemReceived)
--- EVENT_MANAGER:RegisterForEvent("stopCrafting", EVENT_END_CRAFTING_STATION_INTERACT, IdleFPSSaver.OnCraftLeave)
--- EVENT_MANAGER:RegisterForEvent("loadAddon", EVENT_ADD_ON_LOADED, onInitialize)
+EVENT_MANAGER:RegisterForEvent(FPSManager.name, EVENT_ADD_ON_LOADED, FPSManager.OnAddOnLoaded)
