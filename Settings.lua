dc5bcf3085f8442366557d50e051209cc279f484
David Oakley <eso@blott.org>
2024-08-23 10:00:10 +0100

    Use local variable to access savedVars in LoadSettings()

--- Settings.lua
+++ Settings.lua
@@ -3,6 +3,8 @@ local logger = LibDebugLogger(FPSManager.name)
 
 function FPSManager.LoadSettings()
   local LAM = LibAddonMenu2
+  local sv = FPSManager.savedVars
+  if sv == nil then return end
 
   local panelData = {
     type = "panel",
@@ -26,7 +28,7 @@ function FPSManager.LoadSettings()
       tooltip = "Enable to dynamically alter your monitor's refresh rate. Disable to keep it fixed.",
       getFunc = function() return FPSManager.savedVars.enabled end,
       setFunc = function(value)
-        FPSManager.savedVars.enabled = value
+        sv.enabled = value
         FPSManager.OnEnabledChanged(value)
         end,
       width = "full", --or "half",
@@ -38,23 +40,23 @@ function FPSManager.LoadSettings()
     type = "slider",
     name = "Static FPS",
     tooltip = "Refresh rate when Dynamic FPS is disabled",
-    disabled = function() return FPSManager.savedVars.enabled end,
+    disabled = function() return sv.enabled end,
     min = 30,
     max = 240,
     step = 5,	--(optional)
-    getFunc = function() return FPSManager.savedVars.fixedFPS end,
+    getFunc = function() return sv.fixedFPS end,
     setFunc = function(value)
-      FPSManager.savedVars.fixedFPS = value
-      if not FPSManager.savedVars.enabled then
-        logger:Info("FPSManager setting to fixed "..FPSManager.savedVars.fixedFPS.."fps")
-        SetCVar("MinFrameTime.2", ""..(1 / FPSManager.savedVars.fixedFPS))
+      sv.fixedFPS = value
+      if not sv.enabled then
+        logger:Info("FPSManager setting to fixed "..sv.fixedFPS.."fps")
+        SetCVar("MinFrameTime.2", ""..(1 / sv.fixedFPS))
       end
     end,
     width = "full",	--or "half" (optional)
     default = 100,	--(optional)
   })
 
-  local disabledFunc = function() return not FPSManager.savedVars.enabled end
+  local disabledFunc = function() return not sv.enabled end
 
   table.insert(optionsTable, {
     type = "header",
@@ -77,8 +79,8 @@ function FPSManager.LoadSettings()
     min = 10,
     max = 240,
     step = 5,	--(optional)
-    getFunc = function() return FPSManager.savedVars.combatFPS end,
-    setFunc = function(value) FPSManager.savedVars.combatFPS = value end,
+    getFunc = function() return sv.combatFPS end,
+    setFunc = function(value) sv.combatFPS = value end,
     width = "full",	--or "half" (optional)
     default = 60,	--(optional)
     disabled = disabledFunc
@@ -91,8 +93,8 @@ function FPSManager.LoadSettings()
     min = 10,
     max = 240,
     step = 5,	--(optional)
-    getFunc = function() return FPSManager.savedVars.activeFPS end,
-    setFunc = function(value) FPSManager.savedVars.activeFPS = value end,
+    getFunc = function() return sv.activeFPS end,
+    setFunc = function(value) sv.activeFPS = value end,
     width = "full",	--or "half" (optional)
     default = 60,	--(optional)
     disabled = disabledFunc
@@ -105,8 +107,8 @@ function FPSManager.LoadSettings()
     min = 10,
     max = 240,
     step = 5,	--(optional)
-    getFunc = function() return FPSManager.savedVars.idleFPS end,
-    setFunc = function(value) FPSManager.savedVars.idleFPS = value end,
+    getFunc = function() return sv.idleFPS end,
+    setFunc = function(value) sv.idleFPS = value end,
     width = "full",	--or "half" (optional)
     default = 30,	--(optional)
     disabled = disabledFunc
@@ -119,8 +121,8 @@ function FPSManager.LoadSettings()
     min = 10,
     max = 240,
     step = 5,	--(optional)
-    getFunc = function() return FPSManager.savedVars.afkFPS end,
-    setFunc = function(value) FPSManager.savedVars.afkFPS = value end,
+    getFunc = function() return sv.afkFPS end,
+    setFunc = function(value) sv.afkFPS = value end,
     width = "full",	--or "half" (optional)
     default = 10,	--(optional)
     disabled = disabledFunc
@@ -149,8 +151,8 @@ function FPSManager.LoadSettings()
     min = 15,
     max = 20*60,
     step = 15,	--(optional)
-    getFunc = function() return FPSManager.savedVars.idleDelay end,
-    setFunc = function(value) FPSManager.savedVars.idleDelay = value end,
+    getFunc = function() return sv.idleDelay end,
+    setFunc = function(value) sv.idleDelay = value end,
     width = "full",	--or "half" (optional)
     default = 30,	--(optional)
     disabled = disabledFunc
@@ -163,8 +165,8 @@ function FPSManager.LoadSettings()
     min = 60,
     max = 20*60,
     step = 15,	--(optional)
-    getFunc = function() return FPSManager.savedVars.afkDelay end,
-    setFunc = function(value) FPSManager.savedVars.afkDelay = value end,
+    getFunc = function() return sv.afkDelay end,
+    setFunc = function(value) sv.afkDelay = value end,
     width = "full",	--or "half" (optional)
     default = 60,	--(optional)
     disabled = disabledFunc
@@ -183,8 +185,8 @@ function FPSManager.LoadSettings()
       type = "checkbox",
       name = "Show alerts",
       tooltip = "Show an alert in the top-right corner of the screen when FPSManager's state changes",
-      getFunc = function() return FPSManager.savedVars.showAlerts end,
-      setFunc = function(value) FPSManager.savedVars.showAlerts = value end,
+      getFunc = function() return sv.showAlerts end,
+      setFunc = function(value) sv.showAlerts = value end,
       width = "full", --or "half",
       requiresReload = false,
       disabled = disabledFunc
